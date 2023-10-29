e2349d6dcc860a1c94ae886c5c0d4bdbd8253318
Michael <michael.alexander0008@gmail.com>
2023-10-29 22:46:01 +0000

    Complete Exercise 2.11

--- part-2/phonebook/src/App.jsx
+++ part-2/phonebook/src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 import ContactList from './components/ContactList';
 import HTag from './components/HTag';
 import TextualInputDefault from './components/TextualInputDefault';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '040-123456', id: 0 },
-    { name: 'Ada Lovelace', phone: '39-44-5323523', id: 1 },
-    { name: 'Dan Abramov', phone: '12-43-234345', id: 2 },
-    { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 3 }
-  ]); 
+  const [persons, setPersons] = useState([]); 
   const [newName, setNewName] = useState('');
   const [newPhone, setNewPhone] = useState('');
   const [newSearch, setNewSearch] = useState('');
   let [nextID, setNextID] = useState(4);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data));
+  }, [])
 
   const addContact = (e) => {
     e.preventDefault();
